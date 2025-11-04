bd47a5d05c20fb375b63831a38dab3f44d571aa9
Gabriel Carvalho Souza <gcarvalho2112@gmail.com>
2025-11-04 16:43:46 -0300

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MinimalApi.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -16,8 +18,3 @@ app.MapPost("/login", (LoginDTO loginDTO) =>
 app.Run();
 
 
-public class LoginDTO
-{
-    public string Email { get; set; } = default!;
-    public string Password { get; set; } = default!;
-}
